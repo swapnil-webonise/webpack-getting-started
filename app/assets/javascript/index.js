d9514bbac66cc601cc260f4bc8d840b0d84bec7b
Ajay Yadav <ajay.yadav@weboniselab.com>
2015-11-19 17:56:28 +0530

    adding webpack config

---
+++ app/assets/javascript/index.js
@@ -0,0 +1,5 @@
+require("!style!css!./../../../style.css");
+var img = require("!style!css!./../../../style.css");
+console.log("hiii");
+
+const isDeveloping = "dev" !== 'production';
---
+++ webpack.dev.config.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'app/assets/javascript/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name]-[hash].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+     {
+        test: /\.json?$/,
+        loader: 'json'
+      },
+      {
+        test: /\.sass$/,
+        loader: 'style!css!sass?indentedSyntax'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader'
+      }
+
+    ]
+  }
+};
