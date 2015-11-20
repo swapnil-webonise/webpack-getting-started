c9b8f060ab67826b979f47a200fb51a47c093a4f
Ajay Yadav <ajay.yadav@weboniselab.com>
2015-11-20 12:04:40 +0530

    added sass loader

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,50 +6,65 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+var sassLoaders = [
+  'css-loader',
+  'postcss-loader',
+  'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, './app')
+];
+
 module.exports = {
   devtool: 'eval-source-map',
   entry: [
     'webpack-hot-middleware/client?reload=true',
     path.join(__dirname, 'app/assets/javascript/index.js')
   ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+      },
+      {
+        test: /\.json?$/,
+        loader: 'json'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
   output: {
-    path: path.join(__dirname, '/build/'),
-    filename: '[name]-[hash].js',
+    filename: '[name]-[hash].min.js',
+    path: path.join(__dirname, './build'),
     publicPath: '/'
   },
   plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
     new HtmlWebpackPlugin({
       template: 'app/index.tpl.html',
       inject: 'body',
       filename: 'index.html'
     }),
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new ExtractTextPlugin('[name]-[hash].min.css'),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
     })
   ],
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loader: 'babel'
-      },
-     {
-        test: /\.json?$/,
-        loader: 'json'
-      },
-      {
-        test: /\.sass$/,
-        loader: 'style!css!sass?indentedSyntax'
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file-loader'
-      }
-
-    ]
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.sass'],
+    modulesDirectories: ['app', 'node_modules']
   }
 };
